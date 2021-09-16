eb5667229c0d9c5f18ca216f84257c026de20640
mouhcin <mouhcine.qorra@uit.ac.ma>
2021-09-16 15:30:42 +0100

    etwas 8

--- store/views.py
+++ store/views.py
@@ -1,29 +1,21 @@
 from django.shortcuts import render, redirect
 from .models import *
 from getmac import get_mac_address as gma
-import platform, re, uuid
+import platform, getpass
 
 
 
 def store(request):
     ifa = platform.uname()
     try:
-        processor = ifa.processor
-    except:
-        processor = 'None'
-    try:
-        machine = ifa.machine
-    except:
-        machine = 'None'
-    try:
         mc = gma()
     except:
         mc = '0.0.0.0'
     try:
-        system = ifa.system
+        usrnm = (getpass.getuser())
     except:
-        system = 'None'
-    ip = f'mc:{mc};;machine:{machine};;system:{system};;processor:{processor}'
+        usrnm = 'None'
+    ip = f'mc:{mc};;username:{usrnm}'
     try:
         Customer.objects.get(ip=ip)
         print(f'\n\n found ip: {ip}')
